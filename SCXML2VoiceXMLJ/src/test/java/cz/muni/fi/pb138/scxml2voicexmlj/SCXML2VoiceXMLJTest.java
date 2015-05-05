b0d38c7f2acdf2c50fe0761d0e60f3d4166b5819
Petr <petr.domkar@gmail.com>
2015-05-05 19:24:49 +0200

    Přídání souboru testů

---
+++ SCXML2VoiceXMLJ/src/test/java/cz/muni/fi/pb138/scxml2voicexmlj/SCXML2VoiceXMLJTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.scxml2voicexmlj;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Petr
+ */
+public class SCXML2VoiceXMLJTest {
+    
+    public SCXML2VoiceXMLJTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class SCXML2VoiceXMLJ.
+     */
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        SCXML2VoiceXMLJ.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
