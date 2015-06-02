98b14cdd017cc23ee6fc094dc0f7471eb114c7b1
Petr Mejzlík <petr.mejzlik@mail.muni.cz>
2015-06-03 00:06:33 +0200

    SrgsImpl (not complete yet); tests; 2 new example scxml files with inline grammars

--- SCXML2VoiceXMLJ/src/main/java/cz/muni/fi/pb138/scxml2voicexmlj/srgs/Srgs.java
+++ SCXML2VoiceXMLJ/src/main/java/cz/muni/fi/pb138/scxml2voicexmlj/srgs/Srgs.java
@@ -15,8 +15,13 @@ import java.util.Map;
 public interface Srgs {
     /**
      * @param scxmlContent      xml containing the dialog in the scxml format
+     * 
+     * @param grxmlFileName     path to the grxml file where all SRGS grammars 
+     *                          contained directly in {@code scxmlContent} 
+     *                          (not in external grxml files) will be put
+     * 
      * @return                  for each state id, the XML element code 
      *                          to reference the SRGS grammar of that state
      */
-    Map<String,String> getSrgsReferences(InputStream scxmlContent);
+    Map<String,String> getSrgsReferences(InputStream scxmlContent, String grxmlFileName);
 }
